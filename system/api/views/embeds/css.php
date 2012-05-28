d7f907921edf17ebb9cb6ab5852dc67d7beebc3f
Christopher Imrie <chris@ckidesign.co.uk>
2012-05-28 23:48:26 +0100

    Cleaned up some redundant files

--- system/api/views/embeds/css.php
+++
@@ -1,6 +0,0 @@
-<?php if(ENVIRONMENT == "production" || ENVIRONMENT == "testing"): ?>
-    <link rel="stylesheet" href="assets/build/css/styles-min.css" />
-<?php else: ?>
-    <link rel="stylesheet" href="assets/src/css/jquery.mobile.css" />
-    <link rel="stylesheet" href="assets/src/css/clip.css" />
-<?php endif; ?>
--- system/api/views/history/index.php
+++
@@ -1,6 +0,0 @@
-<?php foreach($leads as $lead): ?>
-    <li><a href="<?php echo site_url("history/view/".$lead['id']) ?>" data-transition="slide"><?php echo $lead['data']['name'] ?></a></li>
-<?php endforeach; ?>
-<?php if(count($leads) == 0): ?>
-    <li>No data found</li>
-<?php endif; ?>
